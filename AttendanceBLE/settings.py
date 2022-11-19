897ce54dac4f7ab570cd49bea77bdab62c291c7d
NishiReina <s1f101901195@iniad.org>
2022-11-20 00:30:45 +0900

    [fix] requirements4

--- AttendanceBLE/settings.py
+++ AttendanceBLE/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-)yp-@jdcf!-aknnl*qwaah@)5m9)_mpd2$p8v4k9*+2^f(_a28
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
